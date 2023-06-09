eed0fa84586b801dc40b6626ac28e62c8b6550b8
Ayi Sumarna <ayisumarna@gmail.com>
2023-06-10 01:47:56 +0700

    update fungsi2 validasi skema db userb

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,19 +1,22 @@
-# from typing import Optional # not yet used
+# > schemas > users.py
+# from typing import Optional # modul ini belum digunakan
 from pydantic import BaseModel
 from pydantic import EmailStr
 
 
-# properties required during user creation
+# Properti yang diperlukan saat membuat User baru
 class UserCreate(BaseModel):
     username: str  # TODO ganti jadi name atau full_name
     email: EmailStr
     password: str
+    id_rec: str
 
 
 class ShowUser(BaseModel):
     username: str  # TODO ganti jadi name atau full_name
     email: EmailStr
     is_active: bool
+    # id_rec: str
 
-    class Config:  # tells pydantic to convert even non dict obj to json
+    class Config:  # memberi tahu pydantic untuk mengonversi objek non-dictionary menjadi json
         orm_mode = True
